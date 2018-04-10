b040803043934efeba0a95f8b3e5ef48c2775963
Kayvan Vakili <kvakili813@gmail.com>
2018-04-10 18:48:25 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -24,7 +24,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.scan(/(\d+)-(\d+)-(\d+)/)
+  if phone.match(/(\d+)-(\d+)-(\d+)/)
     true
   else
     false
