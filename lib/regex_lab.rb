e9c727b64bfb50002216c1f0aadcf438b19bf389
Kayvan Vakili <kvakili813@gmail.com>
2018-04-10 19:10:14 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -24,7 +24,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/(\d+)-(\d+)-(\d+)/)
+  if phone.match(/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/)
     true
   else
     false
