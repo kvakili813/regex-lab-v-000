e3fa867eb95b9c976c288e78d732d686776cd49a
Kayvan Vakili <kvakili813@gmail.com>
2018-04-10 18:16:24 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,9 +16,9 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-if text.match(/^[A-Z].*\W$/)
-  true
-end
+  if text.match(/^[A-Z].*\W$/)
+    true
+  end
 end
 
 def valid_phone_number?(phone)
